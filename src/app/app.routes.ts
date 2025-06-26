cdb7aa705f72f8afa6ae762d56d55d5baef1bd72
Fred Morrison <frederick.morrison@outlook.com>
2025-06-25 18:36:32 -0600

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+//
+import {HomePage} from './home-page/home-page';
+import {AboutPage} from './about-page/about-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'home', component: HomePage, title: 'Home'},
+  {path: 'about', component: AboutPage, title: 'About'},
+];
